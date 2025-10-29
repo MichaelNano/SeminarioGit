ca5cece216dbea03e850e20fa39bdb9ab914ef52
MichaelNano <michael777dios@gmail.com>
2025-10-29 09:41:59 +0100

    Projecto_Java
    Aqui teneis el primer proyecto

---
+++ michael/src/ala/hola.java
@@ -0,0 +1,12 @@
+package ala;
+
+/**
+ * @author Michael Jaramillo Palacio
+ * @version 1.0
+ */
+public class hola {
+	public static void main(String[] args) {
+		System.out.println("Hola Equipo");
+	}
+
+}
