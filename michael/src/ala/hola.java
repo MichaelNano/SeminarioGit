a0bac86ceabddfeeb3a73e225488a0cf0bb9a7ca
Pablo Ruiz <pablo785@proton.me>
2025-10-29 09:44:06 +0100

    tio Michale es tonto tu

--- michael/src/ala/hola.java
+++ michael/src/ala/hola.java
@@ -7,6 +7,6 @@ package ala;
 public class hola {
 	public static void main(String[] args) {
 		System.out.println("Hola Equipo");
+		System.out.println("Michael es tonto tio, ha subido los .class");
 	}
-
 }
